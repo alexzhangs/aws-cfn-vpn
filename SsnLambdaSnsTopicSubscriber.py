a183644a85f11967815e3632ed1cf9a4ec511c72
Alex <alexzhangs@gmail.com>
2020-05-19 02:27:08 +0800

    new feature: change the node IP through SNS messages

---
+++ SsnLambdaSnsTopicSubscriber.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+# Description:
+#   Maintain the node stack by receiving the SNS messages.
+#   Supported action:
+#     1. Change the node IP:
+#         * Message body: 'changeip'
+
+from __future__ import print_function
+
+import os
+import json
+import boto3
+
+print('Loading function')
+
+def change_ip(stack):
+    new_param = []
+    for p in stack.parameters:
+        if p['ParameterKey'] == 'EipDomain':
+            if p['ParameterValue'] == '':
+                p['ParameterValue'] = 'vpc'
+            else:
+                p['ParameterValue'] = ''
+        new_param.append(p)
+
+    return stack.update(
+        UsePreviousTemplate=True,
+        Parameters=new_param,
+        Capabilities=[
+            'CAPABILITY_IAM'
+        ]
+    )
+
+def lambda_handler(event, context):
+    print('Received event: ' + json.dumps(event))
+    message = event['Records'][0]['Sns']['Message']
+    print('Message body: ' + message)
+
+    cfn = boto3.resource('cloudformation')
+    stack = cfn.Stack(os.getenv('SSN_STACK_ID'))
+
+    message = message.lower()
+    if message == 'changeip':
+        return change_ip(stack)
+    else:
+        pass
